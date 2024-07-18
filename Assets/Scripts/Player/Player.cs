b0337af0b413f41bcf919607087a1c42d2eb4da9
BloodAssassin <rmilos65@gmail.com>
2024-07-18 20:23:03 +0200

    Update

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator anim;
     public float movementSpeed;
     public bool ableToMove;
     private float horizontal, vertical;
 
     void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -26,6 +28,48 @@ public class Player : MonoBehaviour
     {
         //Player Movement
         rb.velocity = new Vector2 (horizontal, vertical) * movementSpeed;
+
+        //Turn Direction
+
+        //Mouse Oriented
+        if (rb.velocity == Vector2.zero)
+        {
+            //Turn Right
+            if (Input.mousePosition.x < Screen.width / 2f && gameObject.transform.localScale.x > 0)
+            {
+                gameObject.transform.localScale = new Vector2(gameObject.transform.localScale.x * -1, gameObject.transform.localScale.y);
+            }
+            //Turn Left
+            else if (Input.mousePosition.x >= Screen.width / 2f && gameObject.transform.localScale.x < 0)
+            {
+                gameObject.transform.localScale = new Vector2(gameObject.transform.localScale.x * -1, gameObject.transform.localScale.y);
+            }
+        }
+
+        //Walking Oriented
+        if (rb.velocity != Vector2.zero)
+        {
+            //Turn Left
+            if (horizontal > 0 && gameObject.transform.localScale.x < 0)
+            {
+                gameObject.transform.localScale = new Vector2(gameObject.transform.localScale.x * -1, gameObject.transform.localScale.y);
+            }
+            //Turn Right
+            else if (horizontal < 0 && gameObject.transform.localScale.x > 0)
+            {
+                gameObject.transform.localScale = new Vector2(gameObject.transform.localScale.x * -1, gameObject.transform.localScale.y);
+            }
+        }
+
+        //Walking Animation
+        if (rb.velocity != Vector2.zero)
+        {
+            anim.SetBool("Walking", true);
+        }
+        else if (rb.velocity == Vector2.zero)
+        {
+            anim.SetBool("Walking", false);
+        }
     }
 
     private void Move()
